fe61a4fd10bff58ad83845a5abfd65502c2ed1bb
Cole Knowlden <cole@Coles-MacBook-Pro.local>
2015-08-21 20:56:55 -0500

    init commit

---
+++ videoconference/manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    import dotenv
+    dotenv.read_dotenv()
+    
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "videoconference.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
